a91e5bfb2769463d638dfacfeb8f98170b397eb8
Gabriel F. Liriano <gabrielfco28@gmail.com>
2025-07-17 00:20:09 -0400

    Abandoned Global Variables. Moved To Class Handling The JSON String.

--- core.cpp
+++ core.cpp
@@ -2,137 +2,189 @@
 #include <cctype>
 #include <string>
 
-using namespace std;
-
-int index = 0;
-string jstr = "";
-
-string get_bool(string test = ""){
-    string boolean;
-
-    if (!test.empty()){
-        index = 0;
-        jstr = test;
-    }
-
-    if(jstr[index] == 't'){
-        boolean = "true";
-    } else if (jstr[index] == 'f'){
-        boolean = "false";
-    } else{
-        cout << "ERROR: function not called on 'true' or 'false'." << endl;
-        return "";
-    }
-
-    int current = 0;
-    while (boolean[current] != '\0'){
-        if (boolean[current] != jstr[index]){
-            cout << "ERROR: Characters Don't Match" << endl;
-            return "";
-        }
-        current++;
-        index++;
-    }
-
-    
-    return boolean;
-}
-
-string get_number(string test = ""){
-    string number;
-
-    if (!test.empty()){
-        index = 0;
-        jstr = test;
-    }
-
-    while (jstr[index] != '\0'){
-        if (isdigit(jstr[index])){
-            number += jstr[index];
-        } else if (jstr[index] == '.'){
-            number += jstr[index];
-            if (!isdigit(jstr[++index])){
-                cout << "Expected Number After '.'\n";
-                cout << "Found: " << jstr[index] << endl;
-                return "";
-            }
-            continue;
-        } else if (jstr[index] == ' ' || jstr[index] == ','){
-            return number;
-        } else{
-            cout << "Expected Number: \n";
-            cout << "Found: " << jstr[index] << endl;
-            return "";
-        }
-        ++index;
-    }
-
-    //shouldn't be at end of json string if it isn't a test
-    return (!test.empty()) ? number : "";
-}
-
-string get_string(string test = ""){
-    string result;
-
-    if (!test.empty()){
-        index = 0;
-        jstr = test;
-    }
-
-    if(jstr[index] == '"'){
-        while(jstr[++index] != '"'){
-            if (jstr[index] == '\0') cout << "what";
-            result += jstr[index];
-        }
-    }
-
-    return result;
-}
-
 struct Range{
     int start;
     int end;
 };
 
+class JSTR{       
+    private:
+        std::string content;
+        
+    public:
+        int index;
+        int line;
 
-//get range of the body of an array
-Range get_array(string test = ""){
+        JSTR(std::string test = ""){
+            index = 0;
+            line = 1;
+        }
+        
+        char current(){
+            return content[index];
+        }
+
+        char next(){
+            ++index;
+            if (current() == '\n'){
+                ++line;
+            }
+            return current();
+        }
+
+        char escape(){
+            switch(next()){
+                case '\\':
+                    return current();
+                case 'n':
+                    return '\n';
+                case 'b':
+                    return '\b';
+                case 'f':
+                    return '\f';
+                case 'r':
+                    return '\r';
+                case 't':
+                    return '\t';
+                default:
+                    std::cout << "ERROR: " << "'" << current() << "'" << " Is Not A Valid Escaped Character";
+                    return '\0';
+            }
+        }
+
+        void go_to(int dest){
+            index = dest;
+        }
+
+        //set the variables for a test
+        void set_test(std::string test_case){
+            content = test_case;
+            index = 0;
+            line = 1;
+        }
+
+        //print the characters of an object/array
+        void print_range(Range range){
+            go_to(range.start);
+            while (index <= range.end){
+                std::cout << current(); 
+                index++;
+            }
+            std::cout << std::endl;
+        }
+};
+
+std::string get_bool(JSTR &jstr, std::string test = ""){
+    std::string boolean;
+    if (!test.empty()){
+        jstr.set_test(test);
+    }
+
+    if (jstr.current() == 't') {
+        boolean = "true";
+    } else if (jstr.current() == 'f') {
+        boolean = "false";
+    } else {
+        std::cout << "ERROR: function not called on 'true' or 'false'." << std::endl;
+        return "";
+    }
+
+    for (char character : boolean) {
+        if (jstr.current() != character) {
+            std::cout << "ERROR: Characters Don't Match" << std::endl;
+            return "";
+        }
+        jstr.next();
+    }
+
+    return boolean;
+}
+
+std::string get_number(JSTR &jstr, std::string test = ""){
+    std::string number;
+    if (!test.empty()){
+        jstr.set_test(test);
+    }
+
+    while (jstr.current() != '\0') {
+        if (jstr.current() == ' ' || jstr.current() == ',') return number;
+        if (isdigit(jstr.current()) || jstr.current() == '-' || jstr.current() == '.'){
+            number += jstr.current();
+            jstr.next();
+            continue;
+        } else{
+            std::cout << "ERROR: Expected Digit('0-9'), Negative Sign('-') or Floating Point('.')\n";
+            std::cout << "Found: " << jstr.current() << std::endl;
+            return "";
+        }
+        number += jstr.current();
+        jstr.next();
+    }
+
+    //return number if at end of test string | otherwise '\0' shouldn't be reached 
+    return (!test.empty()) ? number : "";
+}
+
+std::string get_string(JSTR &jstr, std::string test = ""){
+    std::string result;
+
+    if (!test.empty()){
+        jstr.set_test(test);
+    }
+
+    while (jstr.current() != '\0'){
+        if (jstr.next() == '"') return result;
+        if (jstr.current() == '\\'){
+            result += jstr.escape();
+            continue;
+        }
+        result += jstr.current();
+    }
+
+    std::cout << "ERROR: Closing Double Quote('\"') Was Not Found";
+    return "";
+}
+
+Range get_array(JSTR &jstr, std::string test = ""){
     int opening = 0;
     int closing = 0;
 
     if (!test.empty()){
-        index = 0;
-        jstr = test;
-    }
-    
-    int start = index;
-    while(jstr[index] != '\0'){
-        if (jstr[index] == '[') opening++;
-        else if (jstr[index] == ']') closing++;
-        if (opening == closing) return {start, index};
-        ++index;
+        jstr.set_test(test);
     }
 
+    int start = jstr.index;
+    int end;
+
+    while (jstr.current() != '\0'){
+        if (jstr.current() == '[') opening++;
+        else if (jstr.current() == ']') closing++;
+        if (opening == closing) return {start, jstr.index};
+        jstr.next();
+    }
+
+    std::cout << "ERROR: Could Not Find Closing Curly Brace -> ']'" << std::endl;
     return {0, 0};
 }
 
-//get range of the body of an object
-Range get_object(string test = ""){
+Range get_object(JSTR &jstr, std::string test = ""){
     int opening = 0;
     int closing = 0;
 
     if (!test.empty()){
-        index = 0;
-        jstr = test;
-    }
-    
-    int start = index;
-    while(jstr[index] != '\0'){
-        if (jstr[index] == '{') opening++;
-        else if (jstr[index] == '}') closing++;
-        if (opening == closing) return {start, index};
-        ++index;
+        jstr.set_test(test);
     }
 
-    return {0, 0};
+    int start = jstr.index;
+    int end;
+
+    while (jstr.current() != '\0'){
+        if (jstr.current() == '{') opening++;
+        else if (jstr.current() == '}') closing++;
+        if (opening == closing) return {start, jstr.index};
+        jstr.next();
+    }
+
+    std::cout << "ERROR: Could Not Find Closing Curly Brace -> '}'" << std::endl;
+    return {0, 0}; 
 }
--- core.h
+++ core.h
@@ -1,36 +1,46 @@
 #pragma core_h
 #include <iostream>
 
-#ifndef json_variables
 
-#define json_variables
-extern std::string jstr;
-extern int index;
-
-#endif
-
-class JSTR{
-    public:
-        std::string jstr;
-        int index;
-
-        char get_next();
-};
-
-JSTR _jstr;
-
-std::string get_bool(std::string test = "");
-
-std::string get_number(std::string test = "");
-
-std::string get_string(std::string test = "");
 
 struct Range{
     int start;
     int end;
 };
 
+class JSTR{
+    private:
+        std::string content;
+        
+    public:
+        int index;
+        int line;
 
-Range get_array(std::string test = "");
+        JSTR(std::string test = ""){
+            index = 0;
+            line = 1;
+        }        
+        
+        char current();
 
-Range get_object(std::string test = "");
+        char next();
+
+        char escape();
+
+        void go_to(int dest);
+
+        void set_test(std::string test_case);
+
+        void print_range(Range range);
+};
+
+std::string get_bool(JSTR &jstr, std::string test = "");
+
+std::string get_number(JSTR &jstr, std::string test = "");
+
+
+std::string get_string(JSTR &jstr, std::string test = "");
+
+Range get_array(JSTR &jstr, std::string test = "");
+
+Range get_object(JSTR &jstr, std::string test = "");
